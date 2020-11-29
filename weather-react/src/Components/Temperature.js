a10d5878a54a9a4743e494a4fc508415f7c9216a
rmthomas98 <rmthomas1998@gmail.com>
2020-11-29 17:51:59 -0600

    added icon on result

--- weather-react/src/Components/Temperature.js
+++ weather-react/src/Components/Temperature.js
@@ -2,10 +2,29 @@ import React from 'react';
 
 const Temperature = (props) => {
 
+	const randConditions = ["Mist", "Smoke", "Haze", "Dust", "Fog", "Sand", "Dust", "Ash", "Squall", "Tornado"]
+
+	let weatherIcon;
+
+	if (props.condition === 'Clear') {
+		weatherIcon = "fas fa-sun"
+	} else if (props.condition === 'Rain' || props.condition === 'Drizzle') {
+		weatherIcon = "fas fa-cloud-showers-heavy"
+	} else if (props.condition === "Clouds"){
+		weatherIcon = "fas fa-cloud"
+	} else if (props.condition === "Thunderstorm") {
+		weatherIcon = "fas fa-bolt"
+	} else if (randConditions.includes(props.condition)) {
+		weatherIcon = "fas fa-smog"
+	}
+
 	return(
 		<div className="weather-container">
 			<div className={props.temperature === '' ? 'no-show' : "temperature"}>{ props.temperature }</div>
-			<div className="condition">{ props.condition } </div>
+			<div className="condition-container">
+				<i className={ weatherIcon }></i>
+				<div className="condition">{ props.condition } </div>
+			</div>
 		</div>
 	)
 }
