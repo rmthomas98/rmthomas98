d98d65d650a07d44d68661b56d9f4b067057d325
rmthomas98 <rmthomas1998@gmail.com>
2020-12-02 16:13:30 -0600

    new link for github

--- portfolio/src/Components/Nav.js
+++ portfolio/src/Components/Nav.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Nav = () => {
     return(
         <div className="buttons">
-            <a href="https://github.com/rmthomas98/rmthomas98" target="_blank">Github</a>
+            <a href="https://github.com/rmthomas98" target="_blank">Github</a>
             <a href="#projects">Projects</a>
             <a href="#about">About me</a>
         </div>
