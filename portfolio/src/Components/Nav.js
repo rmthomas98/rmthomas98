a3281eaba73625f7f640250ac3324984bb901295
rmthomas98 <rmthomas1998@gmail.com>
2020-12-03 19:16:36 -0600

    smooth scroll

--- portfolio/src/Components/Nav.js
+++ portfolio/src/Components/Nav.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import { Link } from 'react-scroll';
 
 const Nav = () => {
+
     return(
         <div className="buttons">
             <a href="https://github.com/rmthomas98" target="_blank">Github</a>
-            <a href="#projects">Projects</a>
-            <a href="#about">About me</a>
+            <Link to="projects" spy={true} smooth={true} duration={1000}>Projects</Link>
+            <Link to="about" spy={true} smooth={true} duration={1200}>About me</Link>
         </div>
     )
 }
