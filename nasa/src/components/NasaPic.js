f1147d4987c7a3b48c08ef7dfdbb8a12f07a107c
rmthomas98 <rmthomas1998@gmail.com>
2020-12-06 15:31:14 -0600

    finished nasa pic of day section

--- nasa/src/components/NasaPic.js
+++ nasa/src/components/NasaPic.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-scroll';
 
 const api = {
   base: 'https://api.nasa.gov/planetary/apod?api_key=',
@@ -12,7 +13,6 @@ class NasaPic extends React.Component {
   super()
     this.state = {
       url: '',
-      copyright: '',
       desc: '',
       title: '',
       isShowingInfo: false
@@ -27,40 +27,61 @@ class NasaPic extends React.Component {
       this.setState({ url: result.url })
       console.log(result)
     })
+    document.getElementById('underline').style.display = "none";
   };
 
   handleButtonClick = (e) => {
     if (this.state.isShowingInfo) {
       this.setState({
-        copyright: '',
         desc: '',
         title: '',
         isShowingInfo: false
       })
       e.target.innerHTML = "Show Picture Info";
-      document.getElementById('apod').style.backgroundColor = "transparent";
-      document.getElementById('nasa-a').style.display = "none"
+      document.getElementById('underline').style.display = "none";
     } else {
       this.setState({
-        copyright: apiInfo.copyright,
         desc: apiInfo.explanation,
         title: apiInfo.title,
         isShowingInfo: true
       })
       e.target.innerHTML = "Hide Picture Info"
-      document.getElementById('apod').style.backgroundColor = "#000000ce";
-      document.getElementById('nasa-a').style.display = "block"
+      document.getElementById('underline').style.display = "block";
     }
   }
 
   render() {
 
-      let infoTitle = this.state.title === '' ? '' : 'Title: '+ this.state.title;
-      let infoAuthor = this.state.copyright === '' ? '' : 'Author: '+ this.state.copyright;
-      let nasaImage = { backgroundImage: `url(${this.state.url})` }
-
     return(
-      <div>hello</div>
+      <div 
+      name="nasa-pic-of-day" 
+      className="section-nasa-pic-container">
+        <h2>Nasa Astronomy Picture of the Day</h2>
+        <div className="nasa-pic-container">
+          <img 
+          src={ this.state.url } 
+          alt="nasa-apod" 
+          className="nasa-pic" 
+          height="800px"/>
+        <div className="button-container">
+          <Link 
+          to={'info'}
+          smooth={true}
+          duration={400}
+          className="view-info" 
+          onClick={ this.handleButtonClick }>
+            View Picture Info
+          </Link>
+        </div>
+          <h3 id="underline">{ this.state.title }</h3>
+          <p
+          id="info"
+          name="info" 
+          className="nasa-desc">
+            { this.state.desc }
+          </p>
+        </div>
+      </div>
     )
   }
 }
