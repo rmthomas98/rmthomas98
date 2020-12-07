d801e0549a1cae6393e666453a9ad0994aadb648
rmthomas98 <rmthomas1998@gmail.com>
2020-12-06 18:15:03 -0600

    components inserted

--- nasa/src/App.js
+++ nasa/src/App.js
@@ -1,18 +1,52 @@
+import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import NasaPic from './components/NasaPic';
+import News from './components/News';
 
-const App = () => {
-  return(
-    <>
-      <div className="front-page-container">
-        <div className="app">
-          <Header />
+
+class App extends React.Component  {
+
+  constructor() {
+    super()
+    this.state = { articles: []}
+  }
+
+  componentDidMount() {
+    fetch('https://test.spaceflightnewsapi.net/api/v2/articles')
+    .then(res => res.json())
+    .then(result => this.setState({ articles: result }) )
+  }
+
+  render() {
+
+    console.log(this.state.articles)
+
+    return(
+      <>
+        <div className="front-page-container">
+          <div className="app">
+            <Header />
+          </div>
         </div>
-      </div>
-      <NasaPic />
-    </>
-  );
+        <div className="news-container" name="news-container">
+          <h2 class="news-header">Latest Spaceflight News</h2>
+          <div className="article-container">
+            { this.state.articles.map((article, index) => 
+              <News
+                key={ index }
+                title={ article.title }
+                url={ article.url }
+                imageUrl={ article.imageUrl }
+                summary={ article.summary }
+              />
+            )}
+          </div>
+        </div>
+        <NasaPic />
+      </>
+    );
+  }
 };
 
 export default App;
