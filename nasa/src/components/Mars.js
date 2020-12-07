c2de5c10fe92c1f9537ac9fc0f7bdb440c737740
rmthomas98 <rmthomas1998@gmail.com>
2020-12-07 16:06:58 -0600

    new mars component

---
+++ nasa/src/components/Mars.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Mars extends React.Component {
+
+  render() {
+    return(
+      <div name="mars" className="mars-container">
+        <h2>Weather on Mars</h2>
+      </div>
+    )
+  }
+}
+
+export default Mars;
