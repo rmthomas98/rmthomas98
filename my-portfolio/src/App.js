f8d254fac79c5ab579481909b157da5b21064e02
rmthomas98 <rmthomas1998@gmail.com>
2020-12-13 19:03:58 -0600

    working on new portfolio

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,12 +1,55 @@
+import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
+import Skills from './components/Skills/Skills'
 
-const App = () => {
-  return(
-    <div className='container'>
-      <Header />
-    </div>
-  );
+let webHeight = document.querySelector('html').offsetHeight - document.querySelector('html').clientHeight;
+
+class App extends React.Component {
+
+  constructor() {
+    super()
+    this.state = {
+      fillPercentage: 0,
+      scrollPos: window.scrollY,
+      htmlHeight: webHeight,
+      color: 'transparent'
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll);
+    window.addEventListener('resize', this.handleResize)
+  }
+
+  handleResize = () => {
+    webHeight = document.querySelector('html').offsetHeight - document.querySelector('html').clientHeight;
+    this.setState({ htmlHeight: webHeight })
+  }
+
+  handleScroll = () => {
+    webHeight = document.querySelector('html').offsetHeight - document.querySelector('html').clientHeight;
+
+    this.setState({ 
+      htmlHeight: webHeight,
+      scrollPos: window.scrollY,
+      fillPercentage: Math.round((window.scrollY / webHeight) * 100)
+    })
+    if (this.state.scrollPos > 400) {
+      this.setState({ color: '#FFFFFF' })
+    } else {
+      this.setState({ color: 'transparent' })
+    }
+  }
+
+  render() {
+    return(
+      <div className='container'>
+        <Header fill={this.state.fillPercentage} background={this.state.color}/>
+        <Skills />
+      </div>
+    );
+  };
 };
 
 export default App;
--- my-portfolio/src/components/Header/Header.js
+++ my-portfolio/src/components/Header/Header.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './Header.css';
 import resume from './assets/ryanthomas.pdf';
+import { Link } from 'react-scroll';
 
 class Header extends React.Component {
 
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
     this.state = {
       isShowing: false,
       animatingClass: '',
@@ -13,7 +14,8 @@ class Header extends React.Component {
       lineAnimation1: '',
       lineAnimation2: '',
       lineAnimation3: '',
-      frontEndButtons: ''
+      frontEndButtons: '',
+      liAnimation: ''
     }
   }
 
@@ -22,7 +24,6 @@ class Header extends React.Component {
   }
 
   handleScroll = () => {
-    console.log(window.scrollY);
     if (window.scrollY > 0) {
       this.setState({ frontEndButtons: 'hide' })
     } else {
@@ -38,7 +39,8 @@ class Header extends React.Component {
         background: '#00000069',
         lineAnimation1: 'line1in 0.3s ease-in forwards',
         lineAnimation2: '0',
-        lineAnimation3: 'line3in 0.3s linear forwards'
+        lineAnimation3: 'line3in 0.3s linear forwards',
+        liAnimation: 'button-slide-in'
       }) 
     } else {
       this.setState({
@@ -47,39 +49,43 @@ class Header extends React.Component {
         background: '',
         lineAnimation1: 'line1out 0.3s ease-in forwards',
         lineAnimation2: '1',
-        lineAnimation3: 'line3out 0.3s linear forwards'
+        lineAnimation3: 'line3out 0.3s linear forwards',
+        liAnimation: ''
       })
     };
   };
 
   render() {
     return(
-      <div className="header-container" style={{backgroundColor: this.state.background}}>
-        <header>
+      <div className="header-container" name="top" style={{backgroundColor: this.state.background}}>
+        <div className="bar">
+          <div className="filler" style={{width: `${this.props.fill}%`}}></div>
+        </div>
+        <header style={{backgroundColor: this.props.background}}>
           <div className="name">
-            <h1>Ryan Thomas</h1>
-          </div>
-          <div className="burger" onClick={this.handleBurgerClick}>
-            <div className="line1 line" style={{animation: this.state.lineAnimation1 }}></div>
-            <div className="line2 line" style={{opacity: this.state.lineAnimation2}}></div>
-            <div className="line3 line" style={{animation: this.state.lineAnimation3}}></div>
+            <h1><Link to={'top'} duration={500} smooth={true}>Ryan Thomas</Link></h1>
           </div>
         </header>
         <div className={`front-end-dev ${this.state.frontEndButtons}`}>
-          <h2>front end developer</h2>
+          <h2 className="front-end-dev-h2">front end developer</h2>
           <div className="button-container">
             <a href="#" className="view-projects-button">View Projects</a>
             <a href="https://github.com/rmthomas98" target="_blank" className="view-projects-button">View Github</a>
           </div>
         </div>
+        <div className="burger" onClick={this.handleBurgerClick}>
+          <div className="line1 line" style={{animation: this.state.lineAnimation1 }}></div>
+          <div className="line2 line" style={{opacity: this.state.lineAnimation2}}></div>
+          <div className="line3 line" style={{animation: this.state.lineAnimation3}}></div>
+        </div>
         <div className={`main-nav ${this.state.animatingClass}`}>
           <ul>
-             <a href=""><li onClick={this.handleBurgerClick}>Projects</li></a>
-             <a href="#"><li onClick={this.handleBurgerClick}>Skills</li></a>
-             <a href="https://github.com/rmthomas98" target="_blank"><li onClick={this.handleBurgerClick}>Github</li></a>
-             <a href={resume}><li onClick={this.handleBurgerClick}>Resume</li></a>
-             <a href="#"><li onClick={this.handleBurgerClick}>About</li></a>
-             <a href="#"><li onClick={this.handleBurgerClick}>Contact</li></a>
+             <a href=""><li onClick={this.handleBurgerClick} className={this.state.liAnimation}>Projects</li></a>
+             <Link to={'skills'} smooth={true} duration={500} offset={-49}><li onClick={this.handleBurgerClick} className={this.state.liAnimation}>Skills</li></Link>
+             <a href="https://github.com/rmthomas98" target="_blank"><li onClick={this.handleBurgerClick} className={this.state.liAnimation}>Github</li></a>
+             <a href={resume} target="_blank"><li onClick={this.handleBurgerClick} className={this.state.liAnimation}>Resume</li></a>
+             <a href="#"><li onClick={this.handleBurgerClick} className={this.state.liAnimation}>About</li></a>
+             <a href="#"><li onClick={this.handleBurgerClick} className={this.state.liAnimation}>Contact</li></a>
           </ul>
         </div>
       </div>
