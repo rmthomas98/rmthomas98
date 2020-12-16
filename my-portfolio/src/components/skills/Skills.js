275f58452594b97cde3334b8e383a3a56ed3c3f0
rmthomas98 <rmthomas1998@gmail.com>
2020-12-15 21:34:09 -0600

    skills section finished

--- my-portfolio/src/components/skills/Skills.js
+++ my-portfolio/src/components/skills/Skills.js
@@ -6,12 +6,14 @@ class Skills extends React.Component {
   constructor() {
     super()
     this.state = {
-      html: 'block',
+      defaultSkill: 'block',
+      defaultSummary: 'block',
+      html: '',
       css: '',
       js: '',
       react: '',
       animation: 'fade-in',
-      htmlTransform: 'html-transform html-color',
+      htmlTransform: '',
       cssTransfrom: '',
       jsTransform: '',
       reactTransform: ''
@@ -23,6 +25,8 @@ class Skills extends React.Component {
     switch(current) {
       case 'HTML':
         this.setState({
+          defaultSkill: 'none',
+          defaultSummary: 'none',
           html: 'block',
           css: 'none',
           js: 'none',
@@ -36,6 +40,8 @@ class Skills extends React.Component {
         break;
       case 'CSS':
         this.setState({
+          defaultSkill: 'none',
+          defaultSummary: 'none',          
           html: 'none',
           css: 'block',
           js: 'none',
@@ -49,6 +55,8 @@ class Skills extends React.Component {
         break;
       case 'Javascript':
         this.setState({
+          defaultSkill: 'none',
+          defaultSummary: 'none',          
           html: 'none',
           css: 'none',
           js: 'block',
@@ -62,6 +70,8 @@ class Skills extends React.Component {
         break;
       case 'React':
         this.setState({
+          defaultSkill: 'none',
+          defaultSummary: 'none',          
           html: 'none',
           css: 'none',
           js: 'none',
@@ -110,16 +120,18 @@ class Skills extends React.Component {
             onClick={this.handleHover}></i>
           </div>
           <div className="skill">
+            <p style={{display: this.state.defaultSkill}}>Select an Icon Above to See More Info</p>
             <p className={`skill-html ${this.state.animation}`} style={{display: this.state.html}}>HTML</p>
             <p className={`skill-css ${this.state.animation}`} style={{display: this.state.css}}>CSS</p>
             <p className={`skill-js ${this.state.animation}`} style={{display: this.state.js}}>Javascript</p>
             <p className={`skill-react ${this.state.animation}`} style={{display: this.state.react}}>React</p>
           </div>
           <div className="skills-summary">
+            <p style={{display: this.state.defaultSummary}}>This is my current skillset. I am continuing to improve my knowledge on each of these daily.</p>
             <p className={`html-summary ${this.state.animation}`} style={{display: this.state.html}}>Experience writing well thought out, semantic HTML. Also have experience with FlexBox as well as FlexGrid.</p>
             <p className={`css-summary ${this.state.animation}`} style={{display: this.state.css}}>Very confident in my abilities to achieve the best look and design within a webisite using CSS. Have experience using pseudo class transitions as well as keyframe animations.</p>
             <p className={`js-summary ${this.state.animation}`} style={{display: this.state.js}}>Ability to write clean, easily understood, ES6 Javscript code. </p>
-            <p className={`react-summary ${this.state.animation}`} style={{display: this.state.react}}>Solid understaning of React. Ability to create dynamic web pages. Create well thought out, reusable components while keeping a good flow of data using state managment as well as props.</p>
+            <p className={`react-summary ${this.state.animation}`} style={{display: this.state.react}}>Solid understaning of React. Ability to create dynamic web pages. Create well thought out, reusable components while keeping a good flow of data using state management as well as props.</p>
           </div>
         </div>
       </div>
