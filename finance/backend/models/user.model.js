d710c6b0a037cce1f7c2f35bf370470464659124
rmthomas98 <rmthomas1998@gmail.com>
2021-07-22 07:31:10 -0400

    changes

--- finance/backend/models/user.model.js
+++ finance/backend/models/user.model.js
@@ -5,9 +5,9 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
-  username: { type: String, required: true, trim: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  customerID: { type: String, default: "" },
 });
 
 const User = mongoose.model("User", userSchema);
--- finance/backend/routes/signup.js
+++ finance/backend/routes/signup.js
@@ -1,25 +1,31 @@
 const router = require("express").Router();
 let User = require("../models/user.model");
 
-router.route("/add").post((req, res) => {
+router.route("/createAccount").post((req, res) => {
   const firstName = req.body.firstName;
   const lastName = req.body.lastName;
-  const username = req.body.username;
   const email = req.body.email;
   const password = req.body.password;
 
   const newUser = new User({
     firstName,
     lastName,
-    username,
     email,
     password,
   });
 
   newUser
     .save()
-    .then(() => res.json("User added!"))
-    .catch((err) => res.status(400).json("Error: " + err));
+    .then((res) => res.json("user added"))
+    .catch((err) => res.json(err));
+});
+
+router.route("/findUser").post((req, res) => {
+  const email = req.body.email;
+
+  User.findOne({ email: email })
+    .then((user) => res.json(user))
+    .catch((err) => res.json(err));
 });
 
 module.exports = router;
